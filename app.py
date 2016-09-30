6c9508a81bb2d5b259e2d72175c99286e1fb419d
Abdulwahaab710 <Abdulwahaab.ahmed710@gmail.com>
2016-09-29 20:10:47 -0400

    Added try and except

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def temperatureToWords(temp):
         elif (-10 < float(t) < -30):
             tempWords.append('freezing cold. You should wear a jacket')
         elif (0 < float(t) < 15):
-            os.system('say {0}'.format('cold'))
+            # os.system('say {0}'.format('cold'))
             tempWords.append('cold')
         elif (15 < float(t) < 20):
             tempWords.append('warm, Room indoors')
@@ -69,10 +69,17 @@ def getHourly(Json):
 
 def getApiKey(key):
     '''(str) -> (str)'''
-    data = ''
-    with open('apiKeys.json') as data_file:
-        data = json.load(data_file)
-    return data['api'][key]
+    try:
+        data = ''
+        with open('apiKeys.json') as data_file:
+            data = json.load(data_file)
+        return data['api'][key]
+    except KeyError:
+        print 'invalid key, or the key doesn\'t exists'
+        exit()
+    except IOError:
+        print 'invalid file name, or the file doesn\'t exists'
+        exit()
 
 
 def sendSMS(phoneNumbers):
